0c71391d6cc694227307ffe521d87f5467bfe626
Ayoub-fetti <ayoubfetti.dev@gmail.com>
2026-02-03 19:49:18 +0100

    EC-35 User schema (MongoDB)

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { databaseConfig } from './config/database.config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      load: [databaseConfig],
+      isGlobal: true,
+    }),
+    MongooseModule.forRoot(process.env.MONGODB_URI || 'mongodb://localhost:27017/events-db'),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
