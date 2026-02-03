1c50aceb949d22511e043b971096d9fa966737d0
Ayoub-fetti <ayoubfetti.dev@gmail.com>
2026-02-03 15:37:42 +0100

    Configurer ESLint + Prettier (front & back)

--- apps/web/eslint.config.mjs
+++ apps/web/eslint.config.mjs
@@ -1,15 +1,20 @@
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
-  // Override default ignores of eslint-config-next.
+  eslintPluginPrettierRecommended,
+  {
+    rules: {
+      "prettier/prettier": ["error", { endOfLine: "auto" }],
+    },
+  },
   globalIgnores([
-    // Default ignores of eslint-config-next:
     ".next/**",
-    "out/**",
+    "out/**", 
     "build/**",
     "next-env.d.ts",
   ]),
