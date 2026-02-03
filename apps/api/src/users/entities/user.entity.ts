82b5ab5aa339c0c5b8c7fa0c938d56c43f8b52b4
Ayoub-fetti <ayoubfetti.dev@gmail.com>
2026-02-03 20:19:52 +0100

    EC-40 : Role-based access control

--- apps/api/src/users/entities/user.entity.ts
+++ apps/api/src/users/entities/user.entity.ts
@@ -1,5 +1,6 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { Document } from 'mongoose';
+import { Role } from 'src/common/enums/role.enum';
 import * as bcrypt from 'bcrypt';
 
 export type UserDocument = User & Document;
@@ -15,7 +16,7 @@ export class User {
   @Prop({ required: true })
   password: string;
 
-  @Prop({ enum: ['admin', 'participant'], default: 'participant' })
+  @Prop({ enum: Role, default: Role.PARTICIPANT })
   role: string;
 
   @Prop({ default: true })
