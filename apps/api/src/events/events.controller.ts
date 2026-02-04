5245e82882bb57ad029d3edc00bde4d0e82009be
Ayoub-fetti <ayoubfetti.dev@gmail.com>
2026-02-04 21:36:00 +0100

    EC-61 : Publish / Cancel event

--- apps/api/src/events/events.controller.ts
+++ apps/api/src/events/events.controller.ts
@@ -1,7 +1,16 @@
-import { Controller, Post, Put, Delete, Body, Param } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Put,
+  Delete,
+  Body,
+  Param,
+  Patch,
+} from '@nestjs/common';
 import { EventsService } from './events.service';
 import { CreateEventDto } from './dto/create-event.dto';
 import { UpdateEventDto } from './dto/update.event.dto';
+import path from 'path';
 
 @Controller('events')
 export class EventsController {
@@ -15,6 +24,15 @@ export class EventsController {
   @Put(':id')
   update(@Param('id') id: string, @Body() UpdateEventDto: UpdateEventDto) {
     return this.eventsService.update(id, UpdateEventDto);
+  }
+
+  @Patch(':id/publish')
+  publish(@Param('id') id: string) {
+    return this.eventsService.publish(id);
+  }
+  @Patch(':id/cancel')
+  cancel(@Param('id') id: string) {
+    return this.eventsService.cancel(id);
   }
 
   @Delete(':id')
--- apps/api/src/events/events.service.ts
+++ apps/api/src/events/events.service.ts
@@ -1,9 +1,14 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import {
+  BadGatewayException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { CreateEventDto } from './dto/create-event.dto';
 import { UpdateEventDto } from './dto/update.event.dto';
 import { Event, EventDocument } from './entities/event.entities';
+import { Status } from 'src/common/enums/status.enum';
 
 @Injectable()
 export class EventsService {
@@ -26,6 +31,45 @@ export class EventsService {
     }
     return updatedEvent;
   }
+
+  async publish(id: string): Promise<Event> {
+    const event = await this.eventModel.findById(id);
+    if (!event) {
+      throw new NotFoundException('Event not found');
+    }
+    if (event.status !== Status.DRAFT.toString()) {
+      throw new BadGatewayException('Only draft events can be published');
+    }
+    const updatedEvent = await this.eventModel.findByIdAndUpdate(
+      id,
+      { status: Status.PUBLISHED },
+      { new: true },
+    );
+    if (!updatedEvent) {
+      throw new NotFoundException('Event not found');
+    }
+    return updatedEvent;
+  }
+
+  async cancel(id: string): Promise<Event> {
+    const event = await this.eventModel.findById(id);
+    if (!event) {
+      throw new NotFoundException('Event not found');
+    }
+    if (event.status === Status.CANCELED.toString()) {
+      throw new BadGatewayException('Event is already canceled');
+    }
+    const updatedEvent = await this.eventModel.findByIdAndUpdate(
+      id,
+      { status: Status.CANCELED },
+      { new: true },
+    );
+    if (!updatedEvent) {
+      throw new NotFoundException('Event not found');
+    }
+    return updatedEvent;
+  }
+
   async remove(id: string): Promise<void> {
     const result = await this.eventModel.findByIdAndDelete(id);
     if (!result) {
